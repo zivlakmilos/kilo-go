92ebe5af078863e39ae458158dbd673f272c567a
Milos Zivlak <zivlakmilos@gmail.com>
2024-09-06 04:13:57 +0200

    Disable echo

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/zivlakmilos/kilo-go
 
 go 1.22.4
 
-require (
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/term v0.24.0 // indirect
-)
+require golang.org/x/sys v0.25.0
--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+var origTermios *unix.Termios
+
+func enableRawMode() {
+	raw, err := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
+	if err != nil {
+		return
+	}
+
+	origTermios = raw
+
+	raw.Lflag &= ^uint32(unix.ECHO)
+
+	err = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, raw)
+	if err != nil {
+		return
+	}
+}
+
+func disableRawMode() {
+	if origTermios != nil {
+		_ = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, origTermios)
+	}
+}
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	enableRawMode()
+	defer disableRawMode()
+
+	b := make([]byte, 1)
+	for {
+		n, err := os.Stdin.Read(b)
+		if err != nil || n < 1 {
+			return
+		}
+		c := b[0]
+
+		if c == 'q' {
+			break
+		}
+	}
 }
