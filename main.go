7bb2808609eb22e6c50f1bf03b8c0c8cbd6da7a8
Milos Zivlak <zivlakmilos@gmail.com>
2024-09-07 03:54:49 +0200

    Disable ctrl+v

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func enableRawMode() {
 	origTermios = raw
 
 	raw.Iflag &= ^uint32(unix.IXON)
-	raw.Lflag &= ^uint32(unix.ECHO | unix.ICANON | unix.ISIG)
+	raw.Lflag &= ^uint32(unix.ECHO | unix.ICANON | unix.ISIG | unix.IEXTEN)
 
 	err = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, raw)
 	if err != nil {
