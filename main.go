450a9a6a9ba1b9acf45d31a9b395172e56fee37d
Milos Zivlak <zivlakmilos@gmail.com>
2024-09-07 04:14:51 +0200

    Error handling

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ import (
 
 var origTermios *unix.Termios
 
+func die(fn string, err error) {
+	fmt.Fprintf(os.Stderr, "%s: %v", fn, err)
+	os.Exit(1)
+}
+
 func enableRawMode() {
 	raw, err := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
 	if err != nil {
-		return
+		die("IoctlGetTermios", err)
 	}
 
 	origTermios = raw
@@ -28,13 +33,16 @@ func enableRawMode() {
 
 	err = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, raw)
 	if err != nil {
-		return
+		die("IoctlSetTermios", err)
 	}
 }
 
 func disableRawMode() {
 	if origTermios != nil {
-		_ = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, origTermios)
+		err := unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, origTermios)
+		if err != nil {
+			die("IoctlSetTermios", err)
+		}
 	}
 }
 
@@ -45,8 +53,14 @@ func main() {
 	b := make([]byte, 1)
 	for {
 		n, err := os.Stdin.Read(b)
+		if err != nil {
+			if err.Error() != "EOF" {
+				die("Read", err)
+			}
+		}
+
 		c := byte(0)
-		if err == nil && n > 0 {
+		if n > 0 {
 			c = b[0]
 		}
 
