1aecfc39a2293897037bdcdaabc66ef502f8da58
Milos Zivlak <zivlakmilos@gmail.com>
2024-09-07 03:53:48 +0200

    Disable ctrl+s and ctrl+q

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func enableRawMode() {
 
 	origTermios = raw
 
+	raw.Iflag &= ^uint32(unix.IXON)
 	raw.Lflag &= ^uint32(unix.ECHO | unix.ICANON | unix.ISIG)
 
 	err = unix.IoctlSetTermios(int(os.Stderr.Fd()), unix.TCSETS, raw)
